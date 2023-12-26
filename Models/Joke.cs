8d19accd70f0e0f26e9a31cd222601e1cf22a168
Swatch <Swatch@DESKTOP-JELR1EG>
2023-12-26 01:21:51 -0500

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System.CodeDom;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string Jokequestion { get; set; }
+        public string Jokeanswer { get; set; }
+
+        public Joke()
+        {
+                
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+    var dynamicElement = document.getElementById('dynamic-element');
+
+        dynamicElement.addEventListener('click', function () {
+            dynamicElement.style.backgroundColor = '#ffcc00';
+        dynamicElement.style.color = '#2c3e50'; /* Change text color on click */
+    });
+  });
+    </script>
